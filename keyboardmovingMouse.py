1ec1d176e87096ed4a4c1735fe89ba52ca6662f2
Jaakko Virtanen <48696955+jaakko13@users.noreply.github.com>
2020-11-06 19:26:17 +0200

    Keyboard is used to move mouse
    My rasberry pi only has one usb port so i can only use my keyboard or mouse but not both at the same time. Script allows me to move cursor and click with the keyboard. Yes I could have used a bluetooth mouse but nah.

---
+++ keyboardmovingMouse.py
@@ -0,0 +1,57 @@
+from pyautogui import *
+import keyboard
+import pyautogui
+import win32api
+import win32con
+
+
+
+def moveLeft():
+    if keyboard.is_pressed('a'):
+        pyautogui.move(-10,0)
+
+def moveRight():
+    if keyboard.is_pressed('d'):
+        pyautogui.move(10,0)
+
+def moveUp():
+    if keyboard.is_pressed('w'):
+        pyautogui.move(0,-10)
+
+def moveDown():
+    if keyboard.is_pressed('s'):
+        pyautogui.move(0,10)
+
+
+def click():
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    time.sleep(0.01)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+
+
+def RightClick():
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,0,0)
+    time.sleep(0.01)
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP,0,0)
+
+
+
+while keyboard.is_pressed('q') == False: # Press q to close script
+
+    if keyboard.is_pressed('a'):
+        moveLeft()
+
+    if keyboard.is_pressed('d'):
+        moveRight()
+
+    if keyboard.is_pressed('w'):
+        moveUp()
+
+    if keyboard.is_pressed('s'):
+        moveDown()
+
+    if keyboard.is_pressed('e'):
+        click()
+
+    if keyboard.is_pressed('r'):
+        RightClick()
