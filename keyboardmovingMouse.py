96b90d148721ce19fb2fb483b033d0f7a6a720e7
Jaakko Virtanen <48696955+jaakko13@users.noreply.github.com>
2020-11-15 18:38:59 +0300

    Update keyboardmovingMouse.py

--- keyboardmovingMouse.py
+++ keyboardmovingMouse.py
@@ -24,15 +24,11 @@ def moveDown():
 
 
 def click():
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    time.sleep(0.01)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+    pyautogui.click()
 
 
 def RightClick():
-    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,0,0)
-    time.sleep(0.01)
-    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP,0,0)
+    pyautogui.click(button='right')
 
 
 
